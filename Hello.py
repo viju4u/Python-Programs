bfc8fc35f790cce43bb3722167467f72c87d6544
Chahan007 <vijayzaware4u@gmail.com>
2022-01-05 18:54:09 +0530

    Changes in Hello.py

--- Hello.py
+++ Hello.py
@@ -1,50 +1,50 @@
-# import keyword
-# kelist=keyword.kwlist
-# print(len(kelist))
+import keyword
+kelist=keyword.kwlist
+print(len(kelist))
 
-# # x,y,z=input().split()
-# # print(x,y,z)
+# x,y,z=input().split()
+# print(x,y,z)
 
-# val='This'[::-1]
-# print(val)
-# revStr1=""
-# def reverse(s):
-#     revStr=""
-#     for str in s.split():
-#         revStr+=str[::-1]+'$'
-#     return revStr
+val='This'[::-1]
+print(val)
+revStr1=""
+def reverse(s):
+    revStr=""
+    for str in s.split():
+        revStr+=str[::-1]+'$'
+    return revStr
 
 
-# import os
-# import json as js
-# my_details = {
+import os
+import json as js
+my_details = {
 
-#     "Name" : "Lalit Salunkhe",
+    "Name" : "Lalit Salunkhe",
 
-#     "Age" : 28,
+    "Age" : 28,
 
-#     "Job" : True,
+    "Job" : True,
 
-#     }
-# obj=js.dumps(my_details)lis
-# print(obj)
+    }
+obj=js.dumps(my_details)lis
+print(obj)
 
-# #print(os.getcwd())
+#print(os.getcwd())
 
-# lis=[1,2,3,"India",[1,2,3]]
-# print(lis)
+lis=[1,2,3,"India",[1,2,3]]
+print(lis)
 
-# lis.append(24)
-# lis.remove(3)
-# print(lis)        
-# lis.pop()
-# print(lis)
+lis.append(24)
+lis.remove(3)
+print(lis)        
+lis.pop()
+print(lis)
 
-# lis.insert(2,10)
-# print(lis)
+lis.insert(2,10)
+print(lis)
     
-# se=set(1,2,3)
-# print(se)
+se=set(1,2,3)
+print(se)
 
 
 str="This is start for me"
